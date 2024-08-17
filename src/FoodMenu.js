1c56ecc52e46393059a02a5138e9fa7ff71c73dc
vsath <vsathishbl2002@gmail.com>
2024-08-17 15:20:40 +0530

    simple updation in showing menu for respective restaurant

--- src/FoodMenu.js
+++ src/FoodMenu.js
@@ -1,25 +1,33 @@
-import React from "react";
-import './FoodMenu.css'
+import React, { useState } from "react";
+import './FoodMenu.css';
+import img from './images/homepage-hero-bg-img.jpg';
 function FoodMenu(props){
     
+    
     const menu=props.menu;
-    // const menu=[
-    //     {item:"parottaa",amt:20,offer:20},
-    //     {item:"mutton chukka",amt:15,offer:15},
-    //     {item:"peper chicken",amt:10,offer:10}
-
-    // ];
-    return(
-        <div>
-           {menu.map((i,index)=>(
-                    <div key={index}>
-                        <p>{i.item}</p>
-                        <p>{i.amt}</p>
-                        <p>{i.offer}</p>
-
-                        </div>
-                ))}
+    return (
+        
+        <div className="dish-list">
+            {menu.map((m, index) => (
+                <div key={index} className="dish-item">
+                    <img src={m.image} className="dish-image" />
+                    <h3>{m.name}</h3>
+                    <p>{m.dicrp}</p>
+                    <p>{m.amt}</p>
+                    <p>{m.offer}</p>
+                </div>
+            ))}
         </div>
+        
     );
 }
 export default FoodMenu;
+ // const menu=[
+    //     {name:"parottaa",   amt:20,  offer:20,  dicrp:"nothing",  image:"/parotta_image.webp"
+    //     },
+    //     {name:"mutton chukka",    amt:15,    offer:15,    dicrp:"nothing",    image:"/mutton_image.jpg"
+    //     },
+    //     {name:"peper chicken",    amt:10,    offer:10,    dicrp:"nothing",    image:"/paneer_image.jpg"
+    //     }
+
+    // ];
--- src/RestaurentAddress.js
+++ src/RestaurentAddress.js
@@ -6,40 +6,58 @@ import FoodMenu from "./FoodMenu";
 function RestaurentAddress(props){
     const[showMenuOnExploreMore,setshowMenuOnExploreMore]=useState(null);
 
-    const addresses=[
-        {street: "Navappalayam",city: "Tiruvannamalai",state: "Tamil nadu",pin:"606901",
-            menu:[
-                {item:"dosa",amt:20,offer:20},
-                {item:"idly",amt:15,offer:15},
-                {item:"vada",amt:10,offer:10}
+    const aboutRestaurant={
+        IDtvm1:{about:"ethachu  soolu bro antha kadaya pathi"},
+        IDmdri1:{about:"ethachu  soolu bro antha kadaya pathi"}
+    };
 
-            ]
+    const addresses={
+        IDtvm1:{street: "Navappalayam",city: "Tiruvannamalai",state: "Tamil nadu",pin:"606901",
+            menu:[
+                {name:"Parottaa",   amt:20,  offer:20,  dicrp:"Delicious, fresh, and flavorful dishes that blend tradition with modern culinary creativity.",  image:"/parotta_image.webp"
+                },
+                {name:"Mutton Chukka",    amt:15,    offer:15,    dicrp:"Delicious, fresh, and flavorful dishes that blend tradition with modern culinary creativity.",    image:"/mutton_image.jpg"
+                },
+                {name:"Peper Chicken",    amt:10,    offer:10,    dicrp:"Delicious, fresh, and flavorful dishes that blend tradition with modern culinary creativity.",    image:"/paneer_image.jpg"
+                }]
         },
-        {street: "melur",city: "madurai",state: "Tamil nadu",pin:"606901",
+        IDmdri1:{street: "melur",city: "madurai",state: "Tamil nadu",pin:"606901",
             menu:[
-                {item:"parottaa",amt:20,offer:20},
-                {item:"mutton chukka",amt:15,offer:15},
-                {item:"peper chicken",amt:10,offer:10}
-
-            ]
+                {name:"Biriyani",   amt:20,  offer:20,  dicrp:"Delicious, fresh, and flavorful dishes that blend tradition with modern culinary creativity.",  image:"/biriyani2_image.jpg"
+                },
+                {name:"Full meal",    amt:15,    offer:15,    dicrp:"Delicious, fresh, and flavorful dishes that blend tradition with modern culinary creativity.",    image:"/full_meals_image.jpg"
+                },
+                {name:"Panner Masala",    amt:10,    offer:10,    dicrp:"Delicious, fresh, and flavorful dishes that blend tradition with modern culinary creativity.",    image:"/paneer_image.jpg"
+                }]
         }
-    ];
+    };
+
+    const onClickClose=()=>{
+        setshowMenuOnExploreMore(<div></div>);
+       };
+       
 
 const onClickExploreMore=(menu)=>{
-        // props.setshowMenuOnExploreMore(<FoodMenu menu={menu}/>);
-        setshowMenuOnExploreMore(<FoodMenu menu={menu}/>);
+        setshowMenuOnExploreMore((
+            <div className="Whole-dish-list">
+                <button className="close-button" onClick={onClickClose}><p>X</p></button>
+                <FoodMenu menu={menu} />
+            </div>
+    ));
         console.log(menu);
    };
+
    
 
     return (
         <div>
         <div className="RestaurentAddressWhole">
-            {addresses.map((adrs,index)=>(
-                <div key={index} className="RestaurentAddresCard">
+            {Object.entries(addresses).map(([id,adrs])=>(
+                <div key={id} className="RestaurentAddresCard">
                 <div className="InsideCard">
                     <img className="Image"src={img}/>
                     <div className="AddressName">
+                        <p>{aboutRestaurant[id].about}</p>
                         <p>{adrs.street}</p>
                         <p>{adrs.city}</p>
                         <p>{adrs.state}</p>
